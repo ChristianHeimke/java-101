47d62f596c4263ddd6aef2eb6f8fa14b2de1838b
Christian Heimke <christian.heimke@loumaris.com>
2022-03-15 12:46:03 +0100

    added case style examples

---
+++ 002-hello-clean-code/src/CaseStyles.java
@@ -0,0 +1,26 @@
+/***************************************************************************************************
+        case styles
+
+        * Variablen sollten sprechende Namen haben
+        * Variablen sollten in einem Stil (camelCase oder snake_case) verwenden
+
+***************************************************************************************************/
+public class CaseStyles {
+  public static void main(String[] args) {
+    // camelCase
+    int thisIsCamelCase = 0;
+    int javaBasicDevOpsKurs = 0;
+
+    // PascalCase == UpperCamelCase
+    int ThisIsAPascalCase = 0;
+    int JavaBasicDevOpsKurs = 0;
+
+    // snake_case
+    int this_is_snake_case = 0;
+    int java_basic_devops_kurs = 0;
+
+    // kebab Case → geht bei java nicht
+    // int this-is-kebab-case = 0;
+    // int java-basic-devops-kurs = 0;
+  }
+}
