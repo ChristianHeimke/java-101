e75089a39df8bad5681f059cc7def94e7913587b
Christian Heimke <christian.heimke@loumaris.com>
2022-04-12 11:53:48 +0200

    added some comments

--- 024-calculator/src/CalculatorTest.java
+++ 024-calculator/src/CalculatorTest.java
@@ -3,14 +3,24 @@ import java.util.Scanner;
 public class CalculatorTest {
 
   public static void main(String[] args) {
+    // Klasse Calculator die die Berechnung übernimmt
+    // auf Basis der Eingabe und Operator
     Calculator c = new Calculator();
+
+    // Erfassen der Eingabe
     Scanner scanner = new Scanner(System.in);
 
+    // Trick für die Endlosschleife - sie geht solange bis der Wert über 0 ist
     int a = -1;
     int b = -1;
+
+    // bei dem operator müssen wir solange warten bis die Eingabe valide ist
     boolean validOperator = true;
+
+    // Operator mit default Wert
     Operator op = Operator.ADD;
 
+    // Schleife für die 1. Zahl
     while (a < 0) {
       try {
         System.out.print("Gib die erste Zahl ein: ");
@@ -20,6 +30,7 @@ public class CalculatorTest {
       }
     }
 
+    // Schleife für die 1. Zahl
     while (b < 0) {
       try {
         System.out.print("Gib die zweite Zahl ein: ");
@@ -29,6 +40,8 @@ public class CalculatorTest {
       }
     }
 
+    // Schleife für den Operator
+    // Fußgesteuert, damit man mind. einmal durch läuft und nach der Operation fragt
     do {
       validOperator = true;
 
@@ -40,8 +53,10 @@ public class CalculatorTest {
         System.out.println("Die Eingabe war ungültig");
         validOperator = false;
       }
-    } while (!validOperator);
+    } while (!validOperator); // schleife läuft solange bis es einen validen Operator gibt
+    // alternative schreibweise: while(validOperator == false)
 
+    // Ausgabe der Berechnung
     try {
       System.out.println(c.calculate(a, b, op));
     } catch (IllegalArgumentException e) {
