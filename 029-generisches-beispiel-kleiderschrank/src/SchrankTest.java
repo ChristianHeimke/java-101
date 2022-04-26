f3a04b14d33f8799b9568de85dbcc4d8405dd20b
Christian Heimke <christian.heimke@loumaris.com>
2022-04-26 12:24:36 +0200

    added comment to test method

--- 029-generisches-beispiel-kleiderschrank/src/SchrankTest.java
+++ 029-generisches-beispiel-kleiderschrank/src/SchrankTest.java
@@ -1,23 +1,32 @@
 public class SchrankTest {
   public static void main(String[] args) {
+
+    // einen Schrank anlegen
     Schrank schrank = new Schrank();
 
+    // ein paar Socken erstellen
     Socken schwarzeSocken = new Socken(44, Farbe.SCHWARZ);
     Socken roteSocken = new Socken(44, Farbe.ROT);
 
+    // ein schwarzes Hemd wird angelegt
     Hemd schwarzesHemd = new Hemd(32, Farbe.SCHWARZ);
 
+    // die socken werden in das entsprechende Fach gelegt
     schrank.fachSocken.einRaeumen(schwarzeSocken);
     schrank.fachSocken.einRaeumen(roteSocken);
+    // ein Hemd gehört nicht in das Sockenfach!
     // schrank.fachSocken.einRaeumen(schwarzesHemd);
 
-    System.out.println(schrank.fachSocken.anzahl());
+    // Ausgabe der Anzahl der Kleidungsstücke
+    System.out.println("Im Kleiderschrank befinden sich: " + schrank.fachSocken.anzahl() + " Kleidungsstücke");
 
-    System.out.println(leeresFach(schrank.fachHemden, schrank.fachSocken));
+    // Prüfen ob es ein leeres Fach gibt
+    System.out.println("Gibt es ein leeres Fach: " + leeresFach(schrank.fachHemden, schrank.fachSocken));
 
 
   }
 
+  // generische wildcard funktion die überprüft ob es ein leeres Fach gibt
   public static boolean leeresFach(Fach<?>... faecher) {
     for (Fach<?> fach : faecher) {
       if (fach.isEmpty()) {
