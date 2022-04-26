d60aff073b4b15de6ddbbae9255d75b21ae7d2ff
Christian Heimke <christian.heimke@loumaris.com>
2022-04-26 08:41:21 +0200

    added template for generic test

---
+++ 029-generisches-beispiel/src/Fach.java
@@ -0,0 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Fach<T> {
+
+}
---
+++ 029-generisches-beispiel/src/SchrankTest.java
@@ -0,0 +1,6 @@
+public class SchrankTest {
+  public static void main(String[] args) {
+
+  }
+
+}
