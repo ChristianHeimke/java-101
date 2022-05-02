0e41ac5164f265fcdbcb16660f369091a9c180bb
Christian Heimke <christian.heimke@loumaris.com>
2022-05-02 12:10:22 +0200

    added equals()

--- 029-generisches-beispiel-kleiderschrank/src/Socken.java
+++ 029-generisches-beispiel-kleiderschrank/src/Socken.java
@@ -1,10 +1,28 @@
 // Klasse Socken, hat nur den Konstruktor zum festlegen der Größe und Farbe
 // erbt alle Methoden von Kleidug
-public class Socken extends Kleidung{
+public class Socken extends Kleidung {
 
   Socken(int groesse, Farbe farbe) {
     this.groesse = groesse;
     this.farbe = farbe;
   }
 
+  @Override
+  public boolean equals(Object obj) {
+
+    if (obj == null) {
+      return false;
+    }
+
+    if (obj.getClass() != this.getClass()) {
+      return false;
+    }
+
+    final Socken other = (Socken) obj;
+    if ((this.farbe == other.farbe) && (this.groesse == other.groesse)) {
+      return true;
+    }
+
+    return true;
+  }
 }
